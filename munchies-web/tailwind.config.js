2768abc66d8c57ef2c20f563399edf6f7a987bb0
Tasreen Mowreen <tmowreen97@gmail.com>
2024-01-19 00:02:35 -0500

    updated tailwind.config

--- munchies-web/tailwind.config.js
+++ munchies-web/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors:{},
+      fontFamily:{
+        poppins: ["Poppins", "sans-serif"],
+      }
+    },
+    screens:{
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl:"1700px",
+    },
   },
   plugins: [],
 }
